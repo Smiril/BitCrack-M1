7fa995ed197d123951ef1ee76949a85a47f97aab
Smiril <Smiril@users.noreply.github.com>
2024-09-07 10:32:15 +0200

    Update launch.sh

--- launch.sh
+++ launch.sh
@@ -1,1 +1,1 @@
-./bin/clBitCrack --keyspace 80000000:FFFFFFFF -i ../../Downloads/btcaddresses1.txt --continue continue1.txt  -o found.txt --stride 1 -b 64 -t 64 -p 32000 -d 0 --compression both 
+./bin/clBitCrack --keyspace 80000000:FFFFFFFF -i ../../Downloads/btcaddresses.txt --continue continue.txt  -o found.txt --stride 1 -b 64 -t 64 -p 32000 -d 0 --compression both 
